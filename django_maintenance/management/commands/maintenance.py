82492fca5042edacc665ce37a2bc04cd6e0c00fb
Ricardo Castro <mcc.ricardo@gmail.com>
2014-08-14 16:38:33 +0100

    Function to set maintenance.

--- django_maintenance/management/commands/maintenance.py
+++ django_maintenance/management/commands/maintenance.py
@@ -7,4 +7,12 @@ JSON_FILENAME = 'django_maintenance/maintenance_settings.json'
 
 class Command(BaseCommand):
     """ Set maintenance status """
-    pass
+    @classmethod
+    def set_maintenance(cls, option):
+    	""" Set maintenance status """
+    	json_data = json.dumps({"DJANgo_MAINTENANCE": option},
+    							indent=4, separators=(',', ': '))
+    	json_file = open(JSON_FILENAME, 'w')
+    	json_file.write(json_data)
+    	json_file.close()
+    
