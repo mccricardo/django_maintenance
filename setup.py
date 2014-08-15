dcb183889de0cce4d15b3d51766a6a1c209771c1
Ricardo Castro <mcc.ricardo@gmail.com>
2014-08-15 12:30:02 +0100

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 version = '0.1.dev0'
 
 setup(
     name = 'django_maintenance',
-    packages = ['django_maintenance'],
+    packages = packages=find_packages(exclude=['test_app', 'django_maintenance_project']),
     version = version,
     download_url = 'https://github.com/mccricardo/django_maintenance/tarball/0.1.dev0'
     )
