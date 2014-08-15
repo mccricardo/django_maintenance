8b07d898230c1bffe27218ef7a2e4d2bb741e747
Ricardo Castro <mcc.ricardo@gmail.com>
2014-08-15 12:24:02 +0100

    Added initial setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+version = '0.1.dev0'
+
+setup(
+    name = 'django_maintenance',
+    packages = ['django_maintenance']
+    version=version,
+    download_url = 'https://github.com/mccricardo/django_maintenance/tarball/0.1.dev0'
+    )
