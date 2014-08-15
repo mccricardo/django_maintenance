5a3d0235c9bde317ddcaa1bfb0ee102997b90b59
Ricardo Castro <mcc.ricardo@gmail.com>
2014-08-15 12:30:32 +0100

    Fixed setup.py bug

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ version = '0.1.dev0'
 
 setup(
     name = 'django_maintenance',
-    packages = packages=find_packages(exclude=['test_app', 'django_maintenance_project']),
+    packages = find_packages(exclude=['test_app', 'django_maintenance_project']),
     version = version,
     download_url = 'https://github.com/mccricardo/django_maintenance/tarball/0.1.dev0'
     )
