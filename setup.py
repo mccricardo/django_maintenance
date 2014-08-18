9dc69a335a9fba50a7e577587e7d8d6cd7308be7
Ricardo Castro <mcc.ricardo@gmail.com>
2014-08-18 12:43:59 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 
-version = '0.1.dev0'
+version = '0.1'
 
 setup(
     name = 'django_maintenance',
     packages = find_packages(exclude=['test_app', 'django_maintenance_project']),
     version = version,
+    description = 'Setting you site Under Maintenance.',
+    author = 'Ricardo Castro',
+    author_email = 'mcc.ricardo@gmail.com',
     download_url = 'https://github.com/mccricardo/django_maintenance',
+    keywords = ['django', 'maintenance'],
     package_data={
       '': ['*.json', 'templates/django_maintenance/*.html'],
    	},
-    )
+)
